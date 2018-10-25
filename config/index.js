fc9f556ab5aef376b419256908ae2787239efc48
朱双龙 <zhushuanglong@zhushuanglongdeMacBook-Pro.local>
2018-10-25 20:13:15 +0800

    up

--- config/index.js
+++ config/index.js
@@ -6,83 +6,83 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'cbs',
+    assetsSubDirectory: '',
     // assetsPublicPath: '//yun.duiba.com.cn/',
-    assetsPublicPath: '//yun.dui88.com/',
+    assetsPublicPath: '//m.waiwaimall.com/',
     productionSourceMap: false, // 设为true生成map可以作调试
     // productionGzip: false,
     // productionGzipExtensions: ['js', 'css'],
     bundleAnalyzerReport: process.env.npm_config_report
   },
   dev: {
-    env: require('./dev.env'),
-    port: 8881,
-    autoOpenBrowser: true, // 自动打开浏览器
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {
-      '/personal/*': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/users/*': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/coupon/*': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/coupons/*': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/orders/*': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/products/*': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/home': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/promotions/*': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/carts': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/cates': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/address/*': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      },
-      '/cards/*': {
-        target: 'http://api.cucoe.net',
-        changeOrigin: true,
-        secure: false
-      }
-    },
-    cssSourceMap: false,
+    // env: require('./dev.env'),
+    // port: 8881,
+    // autoOpenBrowser: true, // 自动打开浏览器
+    // assetsSubDirectory: 'static',
+    // assetsPublicPath: '/',
+    // proxyTable: {
+    //   '/personal/*': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/users/*': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/coupon/*': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/coupons/*': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/orders/*': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/products/*': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/home': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/promotions/*': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/carts': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/cates': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/address/*': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   },
+    //   '/cards/*': {
+    //     target: 'http://api.cucoe.net',
+    //     changeOrigin: true,
+    //     secure: false
+    //   }
+    // },
+    // cssSourceMap: false,
   }
 };
 
