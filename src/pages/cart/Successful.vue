ab7b96d0ca69e0377348dbbbd4a1c00bda8b0d37
朱双龙 <zhushuanglong@zhushuanglongdeMacBook-Pro.local>
2018-10-28 18:19:34 +0800

    up

--- src/pages/cart/Successful.vue
+++ src/pages/cart/Successful.vue
@@ -30,16 +30,15 @@ export default {
       // paypal支付
       payParams = {
         payment_id: this.$route.query.paymentId || '', // paypal返回payment_id
-        payer_id: this.$route.query.payerId || '' // paypal返回payer_id
+        payer_id: this.$route.query.PayerID || '' // paypal返回payer_id
       };
       url = 'PaymentPalExec';
     } else {
       // 银行卡支付
       payParams = {
-        payment_id: this.$route.query.paymentId || '', // paypal返回payment_id
-        payer_id: this.$route.query.payerId || '' // paypal返回payer_id
+        order_id: this.$route.query.orderId || '' // paypal返回payment_id
       };
-      url = 'paymentStatus';
+      url = 'PaymentStatus';
     }
     this.request(url, payParams).then((res) => {
       if (res.status === 200 && res.content) {
