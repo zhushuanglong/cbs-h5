baa0725e2d4048b79d1b605c7abda50b0059ff41
朱双龙 <zhushuanglong@zhushuanglongdeMacBook-Pro.local>
2018-11-10 13:47:36 +0800

    h5

--- src/main.js
+++ src/main.js
@@ -1,44 +1,38 @@
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import initComponents from './initComponents';
-import Http from './libs/http';
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import initComponents from "./initComponents";
+import Http from "./libs/http";
 initComponents(Vue);
 Vue.use(Http);
 // rem 换算
-import { remChange } from './components/basic/remChange';
+import { remChange } from "./components/basic/remChange";
 remChange();
 
 // checkwebp
-import './components/basic/checkwebp';
+import "./components/basic/checkwebp";
 
-import { Switch, Toast, Indicator, Lazyload } from 'mint-ui';
+// 科学计算法 - 加减乘除
+import "./components/basic/math";
+import { returnFloat, accAdd, accSub, accDiv, accMul } from "./components/basic/math";
+Vue.prototype.returnFloat = returnFloat;
+Vue.prototype.accAdd = accAdd;
+Vue.prototype.accSub = accSub;
+Vue.prototype.accDiv = accDiv;
+Vue.prototype.accMul = accMul;
+
+// 组件库
+import { Switch, Toast, Indicator, Lazyload } from "mint-ui";
 Vue.component(Switch.name, Switch);
 Vue.use(Lazyload);
 
 Vue.prototype.$Toast = Toast;
 Vue.prototype.$Indicator = Indicator;
 
-// 保留两位小数
-Vue.prototype.returnFloat = function(value) {
-  value = Math.round(parseFloat(value) * 100) / 100;
-  let xsd = value.toString().split('.');
-  if (xsd.length === 1) {
-    value = value.toString() + '.00';
-    return value;
-  }
-  if (xsd.length > 1 ) {
-    if (xsd[1].length < 2) {
-      value = value.toString() + '0';
-    }
-    return value;
-  }
-}
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
 });
-
