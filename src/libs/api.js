91e61a834880b76e1c48343ee2fe8b5e08c7bed2
wufly <fei.wu@patpat.com>
2018-11-16 10:54:05 +0800

    api.js加入忽略

--- src/libs/api.js
+++
@@ -1,7 +0,0 @@
-let domain = '';
-// if(process.env.NODE_ENV === 'production') {
-//   domain = 'https://api.waiwaimall.com'
-// } else {
-//   domain = 'http://api.cucoe.net'
-// }
-export default domain
